cdb9ca7a7aeb38b32bdf0730d13dd9478fb26807
Chidubem George <chidubem.ikeagu@tdafrica.com>
2025-03-21 16:23:13 +0100

    Removed unsed component

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import CartComponent  from '../components/CartComponent';
+
 
 const Checkout = ({cart}) => {
   const [order, setOrder] = useState({ name: '', phone: '', location: '' });
